7d19def27bbf15f9dac5b66b7f770c47a523b795
Emilia Ndilokelwa Weyulu <eweyulu@mpi-inf.mpg.de>
2020-11-29 17:09:38 +0100

    Clean up

--- fct.py
+++ fct.py
@@ -4,14 +4,16 @@
 """
 
 """
-import os, time, sys  
-import glob, pathlib 
+import os
+import pathlib 
 import pandas as pd
-import math
-import matplotlib as mpl  
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import argparse
+
+import rc3
+import basic
+import log
 
 DATADIR = pathlib.Path(os.getcwd())
 
@@ -22,103 +24,7 @@ DEF_RTT = 20 #In ms
 DEF_RATE = 12 #Mbps
 
 
-def get_rc3_fct(rtt, rate, flow_sz):
-    #Get FCT estimation using formula from RC3 paper
-    #https://www.usenix.org/sites/default/files/conference/protected-files/nsdi14_slides_mittal.pdf
-    #Full paper here: 
-    
-    dict_fct = {}
-    initcwnd = INIT_CWND*MSS
-    
-    dict_fct[rtt] = {}
-    dict_fct[rtt][rate] = {}
-    rate_bytes = (rate*1000000)/float(8)
-    bdp = rate_bytes*rtt
-    
-    for pkts in flow_sz:
-        sz = pkts*MSS
-        dict_fct[rtt][rate][sz] = {}
-        tdelay = (MSS/float(rate_bytes))*pkts
-        if sz <= initcwnd:
-            # Diff from paper: Added 1 rtt for connection setup
-            fct = (2*rtt) + tdelay
-        elif sz > initcwnd and sz <= bdp:
-            # Diff from paper: Added 1 rtt for connection setup,
-            # added trans. delay
-            fct = (math.log((sz/float(initcwnd)),2))*rtt + tdelay + rtt
-        else:
-            # Diff from paper: Added 1 rtt for connection setup
-            fct = (math.log((bdp/float(initcwnd)),2))*rtt + tdelay + rtt
-
-        dict_fct[rtt][rate][sz]['fct'] = fct
-                
-    return dict_fct
-
-
-def get_basic_fct(rtt, rate, flow_sz): 
-    #Get FCT estimation using basic calculations 
-    
-    dict_fct = {}
-    initcwnd = INIT_CWND*MSS
-    
-    dict_fct[rtt] = {}
-    dict_fct[rtt][rate] = {}
-    rate_bytes = (rate*1000000)/float(8)
-    
-    count = 0
-    for pkts in flow_sz:
-        sz = pkts*MSS
-        dict_fct[rtt][rate][sz] = {}
-        tdelay = (MSS/float(rate_bytes))*pkts
-#             print('pkts: {}, TD: {}'.format(pkts,TD))
-        if sz <= initcwnd:
-            fct = (2*rtt) + tdelay
-            tmp_fct = fct
-            count+=1
-        elif sz > initcwnd and tdelay < rtt:
-            trans_time = count * (MSS/float(rate_bytes)) + (MSS/float(rate_bytes))
-            fct = trans_time + (2*rtt) + (1/2*rtt)
-            count+=1
-        else:
-            fct = tmp_fct + tdelay + (1/2* rtt)
-    
-        dict_fct[rtt][rate][sz]['fct'] = fct
-    return dict_fct
-
-
-def get_log_fct(rtt, rate, flow_sz): 
-    #Get FCT estimation using log-based calculations 
-    
-    dict_fct = {}
-    initcwnd = INIT_CWND*MSS
-    
-    dict_fct[rtt] = {}
-    dict_fct[rtt][rate] = {}
-    rate_bytes = (rate*1000000)/float(8)
-    bdp = rate_bytes*rtt
-    
-    for pkts in flow_sz:
-        sz = pkts*MSS
-        dict_fct[rtt][rate][sz] = {}
-        tdelay = (MSS/float(rate_bytes))*pkts
-#             print('pkts: {}, TD: {}'.format(pkts,TD))
-    for pkts in flow_sz:
-        sz = pkts*MSS
-        dict_fct[rtt][rate][sz] = {}
-        tdelay = (MSS/float(rate_bytes))*pkts
-        if sz <= initcwnd:
-            fct = (2*rtt) + tdelay
-        elif sz > initcwnd and sz < bdp:
-            fct = ((2+math.ceil(math.log(sz/float(initcwnd),2)))*rtt) + tdelay
-        else:
-            fct = ((1+(math.ceil(math.log((bdp+(initcwnd-MSS))/float(initcwnd),2))))*rtt) + tdelay
-        
-        dict_fct[rtt][rate][sz]['fct'] = fct
-    
-    return dict_fct
-
-
-def get_plots_no_estimate(*args):
+def plot_no_estimate(*args):
     #Plot FCT results with no FCT estimations
     
     reno_df = args[0]
@@ -152,7 +58,7 @@ def get_plots_no_estimate(*args):
     filename = DATADIR / str('reno-cubic-ns3-d{}b{}q{}-'.format(delay,rate,que)+'.png')
     fig.savefig(str(filename), dpi=300)
 
-def get_plots(*args):
+def plot_with_estimate(*args):
     #Plot FCT results with FCT estimations
     
     reno_df = args[0]
@@ -212,9 +118,9 @@ def main(args):
     cubic_df = pd.read_csv(cubic_fname, header=None, delimiter=' ')
     
     rtt = (args.rtt)/1000 #in seconds
-    rc3_fct= get_rc3_fct(rtt, rate, flow_sz)
-    fct_basic = get_basic_fct(rtt, rate, flow_sz)
-    fct_log = get_log_fct(rtt, rate, flow_sz)
+    rc3_fct= rc3.get_rc3_fct(rtt, rate, flow_sz)
+    fct_basic = basic.get_basic_fct(rtt, rate, flow_sz)
+    fct_log = log.get_log_fct(rtt, rate, flow_sz)
     
     #Names for the plots
     rc3_name = 'rc3'
@@ -222,23 +128,23 @@ def main(args):
     log_name = 'log'
     
     if args.all:
-        get_plots(reno_df, cubic_df, rc3_fct,
+        plot_with_estimate(reno_df, cubic_df, rc3_fct,
                   delay,rate,que, rc3_name)
-        get_plots(reno_df, cubic_df, fct_basic,
+        plot_with_estimate(reno_df, cubic_df, fct_basic,
                   delay,rate,que,basic_name)
-        get_plots(reno_df, cubic_df, fct_log,
+        plot_with_estimate(reno_df, cubic_df, fct_log,
                   delay,rate,que,log_name)
     elif args.rc3:
-        get_plots(reno_df, cubic_df, rc3_fct,
+        plot_with_estimate(reno_df, cubic_df, rc3_fct,
                   delay,rate,que, rc3_name)
     elif args.basic:
-        get_plots(reno_df, cubic_df, fct_basic,
+        plot_with_estimate(reno_df, cubic_df, fct_basic,
                   delay,rate,que,basic_name)
     elif args.log:
-        get_plots(reno_df, cubic_df, fct_log,
+        plot_with_estimate(reno_df, cubic_df, fct_log,
                   delay,rate,que,log_name)
     elif args.plot_ns3:
-        get_plots_no_estimate(reno_df, cubic_df,
+        plot_no_estimate(reno_df, cubic_df,
                   delay,rate,que)
         # sys.exit('Enter FCT estimation option')
 
